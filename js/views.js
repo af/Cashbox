1a8f09be7a220be454925df337da1a962e7de5d2
Aaron Franks <aaron.franks@gmail.com>
2012-01-11 19:35:19 -0800

    Properly delegate events in AppView.

--- js/views.js
+++ js/views.js
@@ -3,6 +3,7 @@
 var CASH = window.CASH = (window.CASH || {});
 CASH.views = CASH.views || {};
 
+
 CASH.views.TableView = Backbone.View.extend({
     el: $('#expenses'),
     template: Handlebars.compile($('#expense_list').text()),
@@ -18,15 +19,35 @@ CASH.views.TableView = Backbone.View.extend({
 
 
 // Main view for the app.
+// Sets up our initial collection and handles file upload events.
 CASH.views.AppView = Backbone.View.extend({
     el: $('body'),
     drop_zone: this.$('#drop_zone'),
     hover_layer: this.$('#hover_cover'),
 
     events: {
-        'change input[type=file]': 'handle_file_upload'
+        // Handle change events on file inputs as if they were uploads:
+        'change input[type=file]': 'handle_file_upload',
+
+        // Drag and drop events:
+        'dragenter':            'show_drop_zone',
+        'dragover #drop_zone':  'cancel_event',
+        'dragleave #drop_zone': 'hide_drop_zone',
+        'dragenter #drop_zone': 'show_hover_layer',
+        'drop      #drop_zone': 'handle_file_drop',
     },
 
+    initialize: function() {
+        var table;
+        _.bindAll(this);
+
+        this.collection = new CASH.models.ImportedExpenses();
+        table = new CASH.views.TableView({ collection: this.collection });
+        this.collection.fetch();
+    },
+
+    // Handle a file upload event (from a file drop or a FileInput),
+    // and pass it to a CSVParser for processing
     handle_file_upload: function(e, file_list) {
         var self = this;
         file_list = file_list || e.target.files;
@@ -35,41 +56,37 @@ CASH.views.AppView = Backbone.View.extend({
         });
     },
 
-    initialize: function() {
-        var self = this,
-            table;
+    // Helper method for cancelling drag and drop events:
+    cancel_event: function(e) { e.stopPropagation(); e.preventDefault(); },
 
-        this.collection = new CASH.models.ImportedExpenses();
-        table = new CASH.views.TableView({ collection: this.collection });
-        this.collection.fetch();
+    // Only reveal the drop zone and the hover layer when a file is dragged over the page:
+    show_drop_zone: function(e) {
+        this.cancel_event(e);
+        this.drop_zone.show();
+    },
 
-        // Basic drag & drop support:
-        var cancel_event = function(e) { e.stopPropagation(); e.preventDefault(); };
+    // Hide the full-page element we use for capturing drop events, so that it
+    // doesn't hijack all other events from the rest of the page.
+    hide_drop_zone: function(e) {
+        this.cancel_event(e);
+        this.hover_layer.hide();
+        this.drop_zone.hide();
+    },
 
-        // Only reveal the drop zone and the hover layer when a file is dragged
-        // over the page:
-        document.body.addEventListener('dragenter', function(e) {
-            cancel_event(e);
-            self.drop_zone.show();
-        });
+    // Shows a full-page layer over the app when dragging a file over it.
+    show_hover_layer: function(e) {
+        this.cancel_event(e);
+        this.hover_layer.show();
+    },
 
-        this.drop_zone[0].addEventListener('dragover', cancel_event);
-        this.drop_zone[0].addEventListener('dragenter', function(e) {
-            cancel_event(e);
-            self.hover_layer.show();
-        });
-        this.drop_zone[0].addEventListener('dragleave', function(e) {
-            cancel_event(e);
-            self.hover_layer.hide();
-            self.drop_zone.hide();
-        });
-        this.drop_zone[0].addEventListener('drop', function(e) {
-            cancel_event(e);
-            self.drop_zone.hide();
-            self.hover_layer.hide();
-            self.handle_file_upload(e, e.dataTransfer.files);
-        });
-    }
+    // Once a file is dropped onto the page, handle the event and process the file data.
+    handle_file_drop: function(e) {
+        var original_evt = e.originalEvent;
+        this.hide_drop_zone(e);
+
+        // Pass the W3C event, which contains the file data (unlike jQuery's wrapper):
+        this.handle_file_upload(original_evt, original_evt.dataTransfer.files);
+    },
 });
 
 }(window, jQuery));
